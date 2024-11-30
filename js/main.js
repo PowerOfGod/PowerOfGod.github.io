671168ceeb64e8151afee2ebd1093cbc1bda8ef9
PowerOfGod <romanovvlad741@gmail.com>
2024-11-30 11:28:48 +0300

    add-slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 let btn = document.querySelector('.menu__btn');
 let menu = document.querySelector('.menu__list');
+
+const images = document.querySelectorAll('.info__slider-img');
+const arrows = document.querySelectorAll('.info__slider-arrow');
+
+let indexSlider = 0;
+
+function show(index) {
+    images[indexSlider].classList.remove('active')
+    images[index].classList.add('active');
+    indexSlider = index;
+}
+
+arrows.forEach((e) => {
+    e.addEventListener('click', (e) => {
+        if(e.target.classList.contains('prev')){
+            let index = indexSlider - 1
+            if(index < 0){
+                index = images.length - 1
+            }
+            show(index);
+        }else if(e.target.classList.contains('next')){
+            let index = indexSlider + 1
+            if(index >= images.length){
+                index = 0
+            }
+            show(index);
+        }
+    } )
+})
+
 btn.addEventListener('click', function(){
     menu.classList.add("hide");
 })
@@ -9,4 +39,5 @@ let close = document.querySelector('.menu__close')
 close.addEventListener('click', () => {
     menu.classList.remove("hide");
 })
-console.log(btn)
+
+
